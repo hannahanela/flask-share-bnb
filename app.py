80e9d29a8b7a9220f0883a3a17ae6baf03a7e685
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-26 17:56:15 -0700

    Created base REST routes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 """Flask app for Share BnB API."""
 
+from pickle import GET
+from webbrowser import get
 from flask import Flask, url_for, render_template, redirect, flash, jsonify
 
 from flask_debugtoolbar import DebugToolbarExtension
@@ -17,6 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
 db.create_all()
+
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -57,5 +60,144 @@ toolbar = DebugToolbarExtension(app)
 # request sent from browser
 # query db
 # return data as JSON
-@app.get("/")
-@app.route[]
+# @app.get("/")
+# @app.route('path',methods=["get"])
+
+
+# TODO: to /api??
+@app.route('/api/listings', methods=["GET"])
+def get_Listings():
+    """
+    Get list of all listings and their data. Returns with JSON:
+        {listings:[{listing}, ...]}
+            listing as {username, img, description, price}
+    """
+
+    # TODO: Create Listing Object
+    listings = Listing.query.all()
+    # TODO: What is serializing again?
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+
+    # TODO:
+    return jsonify(listings=serialized)
+
+
+@app.route("/api/listings/<int:listing_id>", methods=["GET"])
+def get_listing(listing_id):
+    """
+    Get data about a single listing. Returns with JSON:
+        {listing: {username, img, description, price}}
+    """
+    # TODO: Internal route logic
+    listing = Listing.query.get_or_404(listing_id)
+    serialize = listing.serialize()
+
+    return jsonify(listing=serialize)
+
+
+@app.route("/api/listings",methods=["POST"])
+def listing_create():
+    """
+    Creates a listing instance with username, img, description, price and adds
+    to db.
+    # Takes username, img, description, price.
+    Returns with JSON:
+        {listing: {username,img, description, price}}
+    """
+
+    # TODO:
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json.get("image")
+    image = image if image else None
+
+    # alternate method
+    # image = request.get_json()["image"]
+
+    # TODO:
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image=image)
+
+    # TODO:
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    # TODO:
+    serialized = new_cupcake.serialize()
+
+    # TODO:
+    return (jsonify(cupcake=serialized), 201)
+
+
+@app.route('/api/listings/<int:listing_id>',methods=["PATCH"])
+def listing_update_values(listing_id):
+    """
+    Update a listing by id. Any value(s) can be updated
+    TODO: Confirm what can be updated.
+    Updates instance and commits update to database.
+    Returns JSON:
+        {listing: {username,img, description, price}}
+    """
+
+    # TODO:
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    # TODO:
+    flavor = request.json.get("flavor")
+    if flavor:
+        cupcake.flavor = flavor
+
+    size = request.json.get("size")
+    if size:
+        cupcake.size = size
+
+    rating = request.json.get("rating")
+    if rating:
+        cupcake.rating = rating
+
+    image = request.json.get("image")
+    if image:
+        cupcake.image = image
+
+    # TODO: Question on variable attributes:
+    # possible_updates = ['flavor','size','rating','image']
+
+    # for update in possible_updates:
+    #     if update:
+    #         cupcake.(update)
+
+    db.session.commit()
+
+    # TODO: Why dont we re-initialize cupcake after commit?
+    # cupcake = Cupcake.query.get(cupcake_id)
+    serialize = cupcake.serialize()
+    # TODO:
+    return (jsonify(cupcake=serialize))
+
+
+@app.route('/api/listings/<int:listing_id>',methods=["DELETE"])
+def listing_delete(listing_id):
+    """
+    Delete a listing instance and remove from db via id.
+    Takes JSON listing ID.
+    returns JSON {deleted: [listing-id]}
+    """
+
+    # TODO:
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    # TODO:
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    # TODO:
+    return {"deleted":cupcake_id}
+
+
+
+
+
